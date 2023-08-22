0747b8539e8cf4b1e73bbd65c88f4e22c6c07327
goFrendiAsgard <gofrendiasgard@gmail.com>
2023-08-22 08:32:25 +0700

    add playground task

--- zrb_init.py
+++ zrb_init.py
@@ -1,4 +1,22 @@
+from zrb import runner, CmdTask
 import _automate._project as _project
 import _automate.zrb_extras.local as zrb_extras_local
+
 assert _project
 assert zrb_extras_local
+
+playground = CmdTask(
+    name='playground',
+    preexec_fn=None,
+    upstreams=[
+        zrb_extras_local.install_zrb_extras_symlink
+    ],
+    cmd=[
+        'sudo rm -Rf playground',
+        'cp -R playground-template playground',
+        'cd playground',
+        './seed.sh',
+    ]
+)
+runner.register(playground)
+
